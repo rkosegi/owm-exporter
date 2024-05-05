5cc79afbe64ede8557017492eab575e41102723b
Richard Kosegi <richard.kosegi@gmail.com>
2024-05-05 09:14:37 +0200

    Restructure project a bit

---
+++ internal/config.go
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2024 Richard Kosegi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package internal
+
+import (
+	"github.com/rkosegi/owm-exporter/types"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+func LoadConfig(configFile string) (*types.Config, error) {
+	var cfg types.Config
+	data, err := os.ReadFile(configFile)
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(data, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
